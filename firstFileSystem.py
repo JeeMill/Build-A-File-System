ab89353e89c64ab891c08c8625aa937391179f51
Jerler <jeremybertmi@gmail.com>
2024-01-24 10:30:00 -0500

    Building my first file system

---
+++ firstFileSystem.py
@@ -0,0 +1,33 @@
+"""
+Define Node Classes:
+
+1.
+Create a class for a binary tree node (DirectoryNode) to represent directories.
+Create a class for a linked list node (FileNode) to represent files.
+File System Operations:
+
+2.
+Implement functions to create directories (create_directory).
+Implement functions to add files to directories (add_file).
+Implement functions to display files within a directory (display_files).
+Build the File System:
+
+3.
+Create a root directory node.
+Use the functions to create directories and add files to build a file system.
+File System Navigation:
+
+4.
+Implement functionality to navigate through the directories.
+Display files in the current directory.
+"""
+
+
+class DirectoryNode:
+    def __init__(self, directory) -> None:
+        pass
+
+
+class FileNode:
+    def __init__(self, file) -> None:
+        pass
